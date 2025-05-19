abeab5d8010dabd1981b33292500ceb3902be2c7
selholper <selholper@gmail.com>
2025-05-20 01:14:51 +0300

    Linux port fix #2

--- client/src/utils/http.ts
+++ client/src/utils/http.ts
@@ -1,15 +1,24 @@
 import axios from 'axios';
 
+const API_BASE_URL = 'http://localhost:3002';
+axios.defaults.baseURL = API_BASE_URL;
+
 export class Response<Type> {
     constructor(
-        public readonly success: boolean, 
+        public readonly success: boolean,
         public readonly message: Type) { }
 }
 
 export const request = async <Type>(url: string, method: string, data: object={}, params: object={}): Promise<Response<Type>> => {
     try {
-        const res = await axios.request({ url, method, data, params,
-            headers: { 'Content-Type': 'application/json' }, withCredentials: true });
+        const res = await axios.request({
+            url,
+            method,
+            data,
+            params,
+            headers: { 'Content-Type': 'application/json' },
+            withCredentials: true
+        });
         return new Response(true, res.data);
     }
     catch (err) {
--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,10 @@ import router from 'routers/indexRouter';
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: `http://localhost:3000`,
+  credentials: true
+}));
 app.use(cookieParser());
 
 app.use('/', router);
